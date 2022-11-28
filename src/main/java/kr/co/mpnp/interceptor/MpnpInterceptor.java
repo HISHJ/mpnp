b327ceab44a8bd5372d16b9c3d574d7af67188de
CJM-94 <plogwalk@gmail.com>
2022-11-28 22:05:44 +0900

    2022-11-28-22-05 최정민 인터셉터수정

--- src/main/java/kr/co/mpnp/interceptor/MpnpInterceptor.java
+++ src/main/java/kr/co/mpnp/interceptor/MpnpInterceptor.java
@@ -17,101 +17,42 @@ public class MpnpInterceptor implements HandlerInterceptor{
 	  
 	  System.out.println("preHandle 탔음");
 	  System.out.println("id : "+id);
-//	  System.out.println("requestUrl : "+requestUrl);
-	
-	  
+	  System.out.println("requestUrl : "+requestUrl);
 	  // 관리자 로그인 여부
-//	  String adminId = (String)request.getSession().getAttribute("adminId"); 
-////	  // 관리자쪽은 어떻게 식별해서 보낼까?
-////	  // 이렇게되면 
-	  String[] adminPages = {"admin","dashboard"};
-	  //boolean adminFlag = false;
-	  //boolean adminFinalFlag = false;
-	  for(String adminPage : adminPages) {
-		  System.out.println(requestUrl+" 얘랑 "+adminPage+" 얘랑 비교");
-		  if(requestUrl.contains(adminPage)) {
-			  //adminFlag = true;
-			  return true;
-		  }
-	  }
+	  String adminId = (String)request.getSession().getAttribute("adminId"); 
+	  String[] passPages = {"/login","/index","mainList","search","/prd","find","join","admin_login"};
 	  
-//	  if(adminFlag) { // 일단 관리자페이지 일경우
-//		  if(adminId == null) {
-//			  response.sendRedirect("http://localhost/mpnp/3rdAdmin/interceptor_test_admin.jsp");
-//			  //return false;
-//			  adminFinalFlag = false;
-//		  }else if(requestUrl.contains("admin_login")){
-//			  //return true;
-//			  adminFinalFlag = true;
-//		  }
-//	  }
-//	  if(adminFinalFlag) {
-//		  return true;
-//	  }
-	  
-	  
-	  						// id가 없어도 되는애들
-	  String[] notChkPages = {"/login","/index","mainList","search","/prd","find","join"};
 	  boolean passFlag = false;
-	  for(String passPages : notChkPages) { 
-		  System.out.println(requestUrl+" 얘랑 "+passPages+" 얘랑 비교");
-		  if(requestUrl.contains(passPages)) {
+	  for(String page : passPages) {
+		  System.out.println(requestUrl+" 얘랑 "+page+" 얘랑 비교");
+		  if(requestUrl.contains(page)) {
+			  //adminFlag = true;
 			  passFlag = true;
 			  break;
 		  }
 	  }
-	  // 사용자 로그아웃은 어떻게 처리하지??
-	  
-	  // 아이디가 없이들어왔지만 통과가능 페이지이다.
 	  if(passFlag) {
-		  
-		  //System.out.println("로그인안해도 되는페이지임");
 		  return true;
 	  }
+	  // 위의 요청들이 아닌애들
 	  
-	  // 통과가능페이지에서도 걸러졌고 아이디도 null이다.
-	  if(id==null){
-		  //response.sendRedirect("/WEB-INF/views/user/member/login.jsp");
-		  response.sendRedirect("http://localhost/mpnp/3rdDesign/interceptor_test.jsp");
-	      return false; 
+	  if(requestUrl.contains("/admin") || requestUrl.contains("/dashboard")) {
+		  
+		  if(adminId==null) {
+			  response.sendRedirect("http://localhost/mpnp/3rdAdmin/interceptor_test_admin.jsp");
+			  return false;
+		  }else {
+			  return true;
+		  }
 	  }
 	  
-	  // 아이디가 있으며 login,,,find,,,join 이 아닌애들 
-	  // 즉 정상 로그인한 사용자들
-	  return true; 
-	  
-	  
-	  
-	  
-	  // id가 null이 아니다 = 로그인이 되어있다. 그럼 바로 true로 짤라도되잖아
-	  
-	  
-
-
-	  
-	  
-
-	  
-	  
-	  
-	  //로그인 경로 제외 
-//	  if(requestUrl.contains("/login")){ 
-//		  System.out.println("if login 탐");
-//	      return true;
-//	  } 
-//	  //리소스 경로 제외 
-//	  if(requestUrl.contains("/review")){ 
-//		  System.out.println("if review 탐");
-//	      return true;
-//	  } 
-//	  if(requestUrl.contains("review")){ 
-//		  System.out.println("if review 탐");
-//		  return true;
-//	  } 
-	  
-	  
-	  
-	  
+	  // 아이디 없어도 가지는애들이 아니면서 관리자페이지도 아닌경우
+	  if(id==null) {
+		  response.sendRedirect("http://localhost/mpnp/3rdDesign/interceptor_test.jsp");
+	      return false; 
+	  }else {
+		  return true;
+	  }
 	  
 	}
 
---
+++ src/main/webapp/3rdAdmin/interceptor_test_admin.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" info=""%>
+<script type="text/javascript">
+alert("관리자로그인이 필요한 서비스 입니다. 관리자로그인 해주세요.");
+//location.href="login_form.do";
+//location.href="http://localhost/mpnp/login_form.do";
+location.href="http://localhost/mpnp/admin_login_form.do";
+</script>
